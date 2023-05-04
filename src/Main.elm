1f38efefe6e43081f8a711491a0cb8245c4faf0c
Ravi Sankar Karuturi <ravisankarkaruturi@gmail.com>
2023-05-03 23:59:49 -0400

    Implementing elm calender

--- src/Main.elm
+++ src/Main.elm
@@ -19,15 +19,19 @@ main : Program () Model Msg
 main =
    Browser.element
         { init = init
-        , update = updateModel
-        , view = viewModel
+        , update = update
+        , view = view
         , subscriptions = subscriptions
         }
 
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Model Time.utc (Time.millisToPosix 0) False
+  ( Model Time.utc (Time.millisToPosix 0) { year = toYear Time.now
+    , month = toMonth Time.now |> Month.toNumber
+    , selectedDate = Nothing
+    , visible = False
+    }  "2023/05/31 22:25" 
   , Task.perform AdjustTimeZone Time.here
   )
 
@@ -39,8 +43,8 @@ type Msg
     | SelectDate
 
 
-updateModel : Msg -> Model -> -> (Model, Cmd Msg)
-updateModel msg model =
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
     case msg of
         Tick newTime ->
             ( { model | time = newTime }
@@ -69,33 +73,33 @@ updateModel msg model =
                 ( { model | inputText = newInputText }, Cmd.none )
 
 
-viewModel : Model -> Html Msg
-viewModel model =
+view : Model -> Html Msg
+view model =
     let
-    year   = String.fromInt ( Time.toYear model.zone model.time)
-    month   = String.fromInt ( Time.toMonth model.zone model.time)
-    day   = String.fromInt ( Time.toDay model.zone model.time)
-    hour   = String.fromInt (Time.toHour   model.zone model.time)
-    minute = String.fromInt (Time.toMinute model.zone model.time)
-    second = String.fromInt (Time.toSecond model.zone model.time)
-  in
-    div []
-        [ div [] [
-                h1 [] [ text (year ++ "/" ++ month ++ "/" ++ day ++ " "++hour ++ ":" ++ minute ++ ":" ++ second) ] 
-            ]
-        , input [ type_ "text", value model.inputText ] []
-        , button [ onClick SelectDate ] [ text "Select Date & Time" ]
-        , if model.calendarModel.visible then
-                let
-                    calendarView =
-                        Calendar.view model.calendarModel CalendarMsg
-                in
-                div [ class "calendar-popup" ]
-                    [ calendarView ]
+        year   = String.fromInt ( Time.toYear model.zone model.time)
+        month   = ( Time.toMonth model.zone model.time)
+        day   = String.fromInt ( Time.toDay model.zone model.time)
+        hour   = String.fromInt (Time.toHour   model.zone model.time)
+        minute = String.fromInt (Time.toMinute model.zone model.time)
+        second = String.fromInt (Time.toSecond model.zone model.time)
+    in
+        div []
+            [ div [] [
+                    h1 [] [ text (year ++ "/" ++ month ++ "/" ++ day ++ " "++hour ++ ":" ++ minute ++ ":" ++ second) ] 
+                ]
+            , input [ type_ "text", value model.inputText ] []
+            , button [ onClick SelectDate ] [ text "Select Date & Time" ]
+            , if model.calendarModel.visible then
+                    let
+                        calendarView =
+                            Calendar.view model.calendarModel CalendarMsg
+                    in
+                    div [ class "calendar-popup" ]
+                        [ calendarView ]
 
-           else
-                text ""
-        ]
+            else
+                    text ""
+            ]
 
 
 
